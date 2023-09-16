445edbbf983fba25d2377da0955044f29b1e2e84
Sabatier Frédéric, B2C-DCC-DNA <Frederic.Sabatier@swisscom.com>
2023-09-16 12:54:02 +0200

    Basic streamlit chat interface

--- README.md
+++ README.md
@@ -4,6 +4,10 @@ Zurich Hack Challenge
 ## Setup
 PyCharm can setup `pipenv` automatically https://www.jetbrains.com/help/pycharm/pipenv.html#pipenv-requirements
 
+## Running streamlit
+cd hack_zurich_app/webapp
+streamlit run main.py
+
 
 ### Python Tooling
 
--- hack_zurich_app/webapp/main.py
+++ hack_zurich_app/webapp/main.py
@@ -1,3 +1,20 @@
 import streamlit as st
 
-st.title("Placeholder")
+zurich_avatar = "./zurich-logo.png"
+
+st.title("Your personal agent")
+st.caption("Making insurance easy")
+
+if "messages" not in st.session_state:
+    st.session_state["messages"] = [{"role": "assistant", "avatar": zurich_avatar, "content": "How can I help you?"}]
+
+for msg in st.session_state.messages:
+    st.chat_message(msg["role"], avatar=msg["avatar"]).write(msg["content"])
+
+if prompt := st.chat_input():
+    st.session_state.messages.append({"role": "user", "avatar": None, "content": prompt})
+    st.chat_message("user").write(prompt)
+
+    msg = {"role": "assistant", "avatar": zurich_avatar, "content": "I don't know... Do you have another question?"}
+    st.session_state.messages.append(msg)
+    st.chat_message(msg["role"], avatar=msg["avatar"]).write(msg["content"])
