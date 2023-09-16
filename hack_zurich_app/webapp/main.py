6d862dab05dda70ca88b6dd44b21345d3184379a
lzongren <lzongren@gmail.com>
2023-09-16 22:44:46 +0200

    Add PYTHONPATH to main.py && secrets management

--- hack_zurich_app/webapp/main.py
+++ hack_zurich_app/webapp/main.py
@@ -1,12 +1,30 @@
 import base64
+import os
+import sys
 
 import streamlit as st
+
+current_dir = os.path.dirname(os.path.abspath(__file__))
+parent_dir = os.path.dirname(current_dir)
+parent_dir = os.path.dirname(parent_dir)
+
+if parent_dir not in sys.path:
+    sys.path.append(parent_dir)
+
 
 from hack_zurich_app import file_utils
 from hack_zurich_app.agents.support_answer import SupportAnswer
 from hack_zurich_app.agents.support_center import SupportCenter
 
+
 zurich_avatar = f"{file_utils.data_dir()}/zurich-logo.png"
+
+
+def secrets_to_env_var():
+    for key, value in st.secrets.aws_credentials.items():
+        os.environ[key] = value
+    for key, value in st.secrets.llm_credentials.items():
+        os.environ[key] = value
 
 
 @st.cache_resource
@@ -27,15 +45,17 @@ def build_message_from_support_answer(answer: SupportAnswer):
 def print_message(msg):
     st.chat_message(msg["role"], avatar=msg["avatar"]).write(msg["content"])
     if "document_path" in msg:
-        display_pdf(msg['document_path'])
+        display_pdf(msg["document_path"])
 
 
 def display_pdf(document_path):
     with open(document_path, "rb") as f:
-        base64_pdf = base64.b64encode(f.read()).decode('utf-8')
+        base64_pdf = base64.b64encode(f.read()).decode("utf-8")
 
-    pdf_display = f'<iframe src="data:application/pdf;base64,{base64_pdf}" ' \
-                  f'width="700" height="1000" type="application/pdf"></iframe>'
+    pdf_display = (
+        f'<iframe src="data:application/pdf;base64,{base64_pdf}" '
+        f'width="700" height="1000" type="application/pdf"></iframe>'
+    )
 
     document_name = document_path.split("/")[-1]
     with st.expander(document_name):
@@ -50,13 +70,20 @@ def refresh():
 
     if "messages" not in st.session_state:
         st.session_state["messages"] = [
-            {"role": "assistant", "avatar": zurich_avatar, "content": "How can I help you?"}]
+            {
+                "role": "assistant",
+                "avatar": zurich_avatar,
+                "content": "How can I help you?",
+            }
+        ]
 
     for msg in st.session_state.messages:
         print_message(msg)
 
     if prompt := st.chat_input():
-        st.session_state.messages.append({"role": "user", "avatar": None, "content": prompt})
+        st.session_state.messages.append(
+            {"role": "user", "avatar": None, "content": prompt}
+        )
         st.chat_message("user").write(prompt)
 
         support_answer = support_center.ask(prompt)
@@ -67,4 +94,5 @@ def refresh():
         print_message(msg)
 
 
+secrets_to_env_var()
 refresh()
